a77f8fc428daa7f284f2eac01e97f1c10f403791
rileycf11 <rilow11@gmail.com>
2025-01-30 15:48:05 +1100

    Update player.py

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.timer = 0
+        Harry is a gay cunt
     
     def shoot(self):
         if self.timer <= 0:
